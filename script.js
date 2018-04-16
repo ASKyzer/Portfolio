9aa08adb629845b00c6157163dc84537c4695f4d
Adrian Kyzer <adriankyzer@gmail.com>
2018-04-16 02:12:44 -0400

    contact form and scroll to js. media query for smaller screens

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$("#c").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact-me").offset().top
+    }, 1000);
+});
+$("#h").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#home").offset().top
+    }, 1000);
+});
+$("#nav-a").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about-me").offset().top -100
+    }, 1000);
+});
+$("#p").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#portfolio").offset().top
+    }, 1000);
+});
